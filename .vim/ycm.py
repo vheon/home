543d5c8981742ee8a35887679d8cb3f57279935b
Andrea Cedraro <a.cedraro@gmail.com>
2014-05-27 21:50:03 +0200

    Add header for std library in ycm.py

--- .vim/ycm.py
+++ .vim/ycm.py
@@ -6,6 +6,18 @@ def FlagsForFile(filename, **kwargs):
     '-Wextra',
     '-Werror'
     '-pedantic',
+    '-I',
+    '.',
+    '-isystem',
+    '/usr/include',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/5.1/include',
+    '-isystem',
+    '/usr/local/include',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/c++/v1',
   ]
 
   data = kwargs['client_data']
@@ -15,6 +27,7 @@ def FlagsForFile(filename, **kwargs):
     flags += ['-xc']
   elif filetype == 'cpp':
     flags += ['-xc++']
+    flags += ['-std=c++11']
   elif filetype == 'objc':
     flags += ['-ObjC']
   else:
