45bcb1e04fb2c14f5372018b9a765269f6b08ef9
Andrea Cedraro <a.cedraro@gmail.com>
2015-01-11 18:59:43 +0100

    Add Fishshell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,44 @@
+# fish color
+set fish_color_autosuggestion --bold green
+set fish_color_command green
+set fish_color_param blue
+set fish_color_quote yellow
+set fish_color_error red
+set fish_color_search_match --background=green
+set fish_pager_color_description yellow
+
+set -x EDITOR vi
+set -x LC_ALL "en_US.UTF-8"
+set -x LANG "en_US.UTF-8"
+set -x JAVA_HOME (/usr/libexec/java_home)
+set -x SCALA_HOME (brew --repository)"/Library/LinkedKegs/scala/libexec"
+set -x GOPATH $HOME/code/go
+set -x GOBIN $GOPATH/bin
+set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
+set -x PATH /usr/local/bin $HOME/bin $GOBIN $PATH
+# append eclim bin to the path, but eclipse is installed by brew cask
+set -x PATH $PATH (readlink /Applications/Eclipse.app)"/../"
+
+set -x fish_greeting ''
+
+function a
+  ls -lrth -G $argv
+end
+
+function ll
+  ls -lh -G $argv
+end
+
+function take
+  mkdir -p $argv[1] and cd $argv[1]
+end
+
+# cd into whatever is the forefront Finder window.
+function cdf # short for cdfinder
+  cd (osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')
+end
+
+function mountg
+  mkdir -p $HOME/tmp/storage
+  sshfs -o volname=Storage -o Compression=no -o Ciphers=arcfour $USER@ghost.local:/mnt/storage $HOME/tmp/storage
+end
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,47 @@
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "!"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+
+set -g __fish_git_prompt_char_cleanstate ""
+
+set -g __fish_git_prompt_color_branch green
+set -g __fish_git_prompt_color_dirtystate magenta
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles cyan
+set -g ___fish_git_prompt_color_merging magenta
+
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
+
+	if test -n "$SSH_CONNECTION"
+		echo -n (set_color green)"$USER"(set_color normal)"@"(set_color magenta)(hostname)" "
+	end
+
+	# PWD
+	set_color blue
+	echo -n (pwd|sed "s|$HOME|~|")
+	set_color normal
+
+	echo -n (__fish_git_prompt " (±)-[%s]")
+
+	# New line
+	echo
+
+	if not test (count (jobs | grep "stopped")) -eq 0
+		set_color yellow
+		echo -n '☰ '
+		set_color normal
+	end
+
+	if test $last_status = 0
+		set_color green
+	else
+		set_color red
+	end
+	echo -n "❯ "
+	set_color normal
+end
