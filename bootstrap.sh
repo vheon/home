50fce01b336537182e223d406fa523eeeb16e589
Andrea Cedraro <a.cedraro@gmail.com>
2021-05-10 01:29:10 +0200

    Only try to install ansible if not present already

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,17 @@ install_ansible_ubuntu()
   sudo apt update && sudo apt install -y ansible
 }
 
-if [[ $(uname) = "Darwin" ]]; then
-  install_ansible_macos
-else
-  install_ansible_ubuntu
+install_ansible()
+{
+  if [[ $(uname) = "Darwin" ]]; then
+    install_ansible_macos
+  else
+    install_ansible_ubuntu
+  fi
+}
+
+if ! command -v ansible-playbook &> /dev/null; then
+  install_ansible
 fi
 
 exit $?
