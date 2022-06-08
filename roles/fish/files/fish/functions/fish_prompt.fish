0e769ea4f87b6db82d2bf83d2ecdabb3989360ea
Andrea Cedraro <andrea.cedraro@hcl.com>
2022-06-08 10:41:20 +0200

    fish: reformat fish_prompt

--- roles/fish/files/fish/functions/fish_prompt.fish
+++ roles/fish/files/fish/functions/fish_prompt.fish
@@ -7,15 +7,15 @@ set base04 a7adba
 set base05 c0c5ce
 set base06 cdd3de
 set base07 d8dee9
-set red    ec5f67
+set red ec5f67
 set orange f99157
 set yellow fac863
-set green  99c794
-set cyan   62b3b2
-set blue   6699cc
+set green 99c794
+set cyan 62b3b2
+set blue 6699cc
 set purple c594c5
-set brown  ab7967
-set white  ffffff
+set brown ab7967
+set white ffffff
 
 set -g fish_color_normal $white
 set -g fish_color_command $white --bold
@@ -48,40 +48,45 @@ set -g __fish_git_prompt_color_invalidstate $red
 set -g __fish_git_prompt_color_untrackedfiles $cyan
 set -g ___fish_git_prompt_color_merging $purple
 
+# Replace $HOME with "~"
+function pwd_tilde_home --description 'short $HOME with ~'
+    set --local realhome ~
+    echo (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+end
+
 function fish_prompt
-  set --local last_status $status
+    set --local last_status $status
 
-  set --local color_normal (set_color normal)
+    set --local color_normal (set_color normal)
 
-  # I'm in a tmux session I have the user@machine is in the tmux status line.
-  # No need to put it also in the prompt.
-  if test -n "$SSH_CONNECTION" && test -z "$TMUX"
-    set --local user_color (set_color $green)
-    set --local user $USER
-    set --local hostname_color (set_color $purple)
-    echo -n "$user_color$USER$color_normal@$hostname_color$hostname$color_normal "
-  end
+    # I'm in a tmux session I have the @machine is in the tmux status line.
+    # No need to put it also in the prompt.
+    if test -n "$SSH_CONNECTION" && test -z "$TMUX"
+        set --local user_color (set_color $green)
+        set --local hostname_color (set_color $purple)
+        echo -n "$user_color$USER$color_normal@$hostname_color$hostname$color_normal "
+    end
 
-  # PWD
-  set --local path_color (set_color $blue)
-  set --local path (pwd|sed "s|$HOME|~|")
-  echo -n "$path_color$path$color_normal"
+    # PWD
+    set --local path_color (set_color $blue)
+    set --local path (pwd_tilde_home)
+    echo -n "$path_color$path$color_normal"
 
-  # git + newline
-  echo (__fish_git_prompt " \uE0A0 %s")
+    # git + newline
+    echo (__fish_git_prompt " \uE0A0 %s")
 
-  if test (count (jobs | grep "stopped")) -ne 0
-    set --local jobs_color (set_color $yellow)
-    # XXX(andrea): right now this char is taking more than 1 cell space :?
-    # which is why I also have a space after printing it...
-    set --local jobs_char '☰'
-    echo -n "$jobs_color$jobs_char$color_normal "
-  end
+    if test (count (jobs | grep "stopped")) -ne 0
+        set --local jobs_color (set_color $yellow)
+        # XXX(andrea): right now this char is taking more than 1 cell space :?
+        # which is why I also have a space after printing it...
+        set --local jobs_char '☰'
+        echo -n "$jobs_color$jobs_char$color_normal "
+    end
 
-  set --local prompt_char '❯'
-  set --local prompt_char_color (set_color $green)
-  if test $last_status -ne 0
-    set prompt_char_color (set_color $red)
-  end
-  echo -n "$prompt_char_color$prompt_char$color_normal "
+    set --local prompt_char '❯'
+    set --local prompt_char_color (set_color $green)
+    if test $last_status -ne 0
+        set prompt_char_color (set_color $red)
+    end
+    echo -n "$prompt_char_color$prompt_char$color_normal "
 end
