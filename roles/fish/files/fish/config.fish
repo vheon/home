06bfd1dc8e0846f4a802e14e018e624a16f31750
Andrea Cedraro <a.cedraro@gmail.com>
2022-09-03 12:29:32 +0200

    fish: move functions to be autoloaded

--- roles/fish/files/fish/config.fish
+++ roles/fish/files/fish/config.fish
@@ -1,18 +1,2 @@
 set -x fish_greeting ''
 set -x fish_term24bit 1
-
-function a
-  ls -lrth -G $argv
-end
-
-function ll
-  ls -lh -G $argv
-end
-
-function take --description 'create a directory and cd into it'
-  mkdir -p $argv[1]; and cd $argv[1]
-end
-
-function cdf --description 'cd into whatever is the forefront Finder window.'
-  cd (osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')
-end
---
+++ roles/fish/files/fish/functions/take.fish
@@ -0,0 +1,3 @@
+function take --description 'create a directory and cd into it'
+  mkdir -p $argv[1]; and cd $argv[1]
+end
