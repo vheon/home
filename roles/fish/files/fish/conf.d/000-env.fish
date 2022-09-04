3d21b816072a407e689c7b02adceffb3d31d2c58
Andrea Cedraro <a.cedraro@gmail.com>
2022-09-04 16:09:05 +0200

    fish: restructure 000-env.fish file to use `fish_add_path`

--- roles/fish/files/fish/conf.d/000-env.fish
+++ roles/fish/files/fish/conf.d/000-env.fish
@@ -1,3 +1,19 @@
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx GOKU_EDN_CONFIG_FILE $XDG_CONFIG_HOME/karabiner/karabiner.edn
+set -gx EDITOR nvim
+set -gx VISUAL $EDITOR
+set -gx SUDO_EDITOR $EDITOR
+set -gx LC_ALL "en_US.UTF-8"
+set -gx LANG "en_US.UTF-8"
+
+set -Ux fish_user_paths
+fish_add_path $HOME/bin
+fish_add_path $XDG_CONFIG_HOME/fzf/bin
+
+# XXX(andrea): This should be set somehow by the go role :/ maybe the role could add a fish file
+set -x GOPATH $HOME/go
+fish_add_path $GOPATH/bin
+
 set -x BREW_PREFIX /usr/local
 if test -d /home/linuxbrew/.linuxbrew
   set -x BREW_PREFIX /home/linuxbrew/.linuxbrew
@@ -5,12 +21,11 @@ end
 if test -d /opt/homebrew
   set -x BREW_PREFIX /opt/homebrew
 end
+fish_add_path $BREW_PREFIX/bin
+fish_add_path $BREW_PREFIX/sbin
 
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x GOKU_EDN_CONFIG_FILE $XDG_CONFIG_HOME/karabiner/karabiner.edn
-set -x EDITOR nvim
-set -x LC_ALL "en_US.UTF-8"
-set -x LANG "en_US.UTF-8"
-set -x GOPATH $HOME/go
-set -u PATH
-set -x PATH $HOME/bin $XDG_CONFIG_HOME/fzf/bin $GOPATH/bin $BREW_PREFIX/bin /bin /usr/bin /usr/local/bin $BREW_PREFIX/sbin /sbin /usr/sbin /usr/local/sbin
+# XXX(andrea): this should be only on linux when snap is installed
+fish_add_path /snap/bin
+
+# XXX(andrea): this is work-related and should be set in a specific role
+fish_add_path $HOME/code/bigfix-builders-images
