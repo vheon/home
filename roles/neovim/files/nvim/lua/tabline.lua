50c15ae089568f0f380683f4f1174c6d3761a0da
Andrea Cedraro <andrea.cedraro@hcl.com>
2022-09-26 09:20:59 +0200

    nvim: update tabline

--- roles/neovim/files/nvim/lua/tabline.lua
+++ roles/neovim/files/nvim/lua/tabline.lua
@@ -1,20 +1,24 @@
+local devicons = require'nvim-web-devicons'
+
 local function highlight(current, tabpage)
   if current == tabpage then
     return "%#TabLineSel#"
-    -- return '%#Normal#'
   end
   return "%#TabLine#"
-  -- return '%#StatusLineNC#'
 end
 
--- local function separator(current, tabpage)
---     if current == tabpage then
---       return '▎'
---     end
---     return ""
--- end
+local function separator(current, tabpage)
+  local line
+    if current == tabpage then
+      line = "  "
+    else
+      line = "  "
+    end
+    return line .. tabpage .. "  "
 
-local function extract_name(win)
+end
+
+local function extract_filename(win)
   local buf = vim.api.nvim_win_get_buf(win)
   local fullname = vim.api.nvim_buf_get_name(buf)
   if fullname ~= "" then
@@ -24,44 +28,56 @@ end
 
 local nil_wrap = vim.F.npcall
 
+local function prepadded(el, n)
+  if el ~= nil and #el > 0 then
+    n = n or 1
+    return (' '):rep(n) .. el
+  end
+  return ''
+end
+
+
 local function getname(tabpage)
   local title = nil_wrap(vim.api.nvim_tabpage_get_var, tabpage, "tab_title")
   if title ~= nil then
     return title
   end
 
-  local fullname = extract_name(vim.api.nvim_tabpage_get_win(tabpage))
-  if fullname ~= nil then
-    return fullname
-  end
-  -- because it was not possible to get the name of the active windows just try
-  -- to grab the first name available.
-  local wins = vim.api.nvim_tabpage_list_wins(tabpage)
-  if #wins > 1 then
-    for _, win in ipairs(wins) do
-      fullname = extract_name(win)
-      if fullname ~= nil then
-        return fullname
+  local filename = extract_filename(vim.api.nvim_tabpage_get_win(tabpage))
+  if filename == nil then
+    -- because it was not possible to get the name of the active windows just try
+    -- to grab the first name available.
+    local wins = vim.api.nvim_tabpage_list_wins(tabpage)
+    if #wins > 1 then
+      for _, win in ipairs(wins) do
+        filename = extract_filename(win)
+        break
       end
     end
   end
-  return "[No Name]"
+
+  if filename == nil then
+    return "[No Name]"
+  end
+
+  local extension = string.match(filename, '%a+$')
+  local icon = devicons.get_icon(filename, extension)
+  return filename .. prepadded(icon)
 end
 
 local function tabline()
-  local line = ""
+  local tl = {}
   local current = vim.api.nvim_get_current_tabpage()
   for i, tabpage in ipairs(vim.api.nvim_list_tabpages()) do
-    line = line .. highlight(current, tabpage)
-    line = line .. "▎"
-    line = line .. "%" .. i .. "T" -- Starts mouse click target region
-    line = line .. getname(tabpage)
-    line = line .. " "
+    table.insert(tl, highlight(current, tabpage))
+    table.insert(tl, separator(current, tabpage))
+    table.insert(tl, "%" .. i .. "T") -- Starts mouse click target region
+    table.insert(tl, getname(tabpage))
+    table.insert(tl, " ")
   end
-  line = line .. "%#TabLineFill#"
-  -- line = line .. '%#StatusLineNC#'
-  line = line .. "%T" -- Ends mouse click target region(s).
-  return line
+  table.insert(tl, "%#TabLineFill#")
+  table.insert(tl, "%T") -- Ends mouse click target region(s).
+  return table.concat(tl, '')
 end
 
 return { line = tabline }
