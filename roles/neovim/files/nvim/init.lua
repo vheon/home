13bc5db73765b37c2ce80cc568bcb29b0ec5a3ca
Andrea Cedraro <andrea.cedraro@hcl.com>
2022-11-05 15:16:23 +0100

    nvim: disable unused plugins

--- roles/neovim/files/nvim/init.lua
+++ roles/neovim/files/nvim/init.lua
@@ -7,6 +7,27 @@ augroup END
 command! -nargs=* Autocmd autocmd VimrcAutocmds <args>
 ]]
 
+-- Disable unused plugins
+vim.g.loaded_gzip = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_2html_plugin = 1
+
+-- XXX(andrea): disable this if I try to use monkoose/matchparen.nvim
+-- vim.g.loaded_matchparen = 1
+-- vim.g.loaded_matchit = 1
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+
 vim.g.mapleader = vim.api.nvim_replace_termcodes("<Space>", false, false, true)
 
 _G.always_require = function(module)
