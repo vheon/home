93a25c9b7e9be1f34b17de56123211ee8653e519
Andrea Cedraro <andrea.cedraro@hcl.com>
2022-11-05 14:55:45 +0100

    iterm2: refactor adjust_font_size script

--- roles/iterm/files/Scripts/adjust_font_size.py
+++ roles/iterm/files/Scripts/adjust_font_size.py
@@ -11,11 +11,21 @@ args = parser.parse_args()
 
 size = args.size
 
+# This regex splits the font into its name and size. Fonts always end with
+# their size in points, preceded by a space.
+font_regex = re.compile(r'^(.* )\d*$')
+
+def extract_font_name(font):
+    match = font_regex.search(font)
+    if not match:
+        return None
+
+    groups = match.groups()
+    return groups[0]
+
+
 async def increase_font_size(connection):
     app = await iterm2.async_get_app(connection)
-    # This regex splits the font into its name and size. Fonts always end with
-    # their size in points, preceded by a space.
-    r = re.compile(r'^(.* )\d*$')
 
     session=app.current_terminal_window.current_tab.current_session
     if not session:
@@ -24,17 +34,14 @@ async def increase_font_size(connection):
     # Get the session's profile because we need to know its font.
     profile = await session.async_get_profile()
 
-    # Extract the name and point size of the font using a regex.
-    font = profile.normal_font
-    match = r.search(font)
-    if not match:
-        return
-    groups = match.groups()
-    name = groups[0]
-
-    replacement = name + str(size)
     change = iterm2.LocalWriteOnlyProfile()
-    change.set_normal_font(replacement)
+
+    # Extract the name and point size of the font
+    name = extract_font_name(profile.normal_font)
+    if not name:
+        return
+
+    change.set_normal_font(name + str(size))
 
     # Update the session's copy of its profile without updating the
     # underlying profile.
