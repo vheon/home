440736b05138b603113268b313e618efa45dc8d1
Andrea Cedraro <andrea.cedraro@hcl.com>
2023-02-10 17:53:57 +0100

    meta: fix the nvim role

--- roles/neovim/files/nvim/lua/config/lazy.lua
+++ roles/neovim/files/nvim/lua/config/lazy.lua
@@ -1,17 +1,4 @@
--- XXX(andrea): this have to go! Since I manage my dotfiles with ansible there is no reason
--- to check if lazy is in place and download it from here
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    }
-end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.fn.stdpath "data" .. "/lazy/lazy.nvim")
 
 require("lazy").setup("plugins", {
     install = {
