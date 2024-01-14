e428d2e30ab2ebe80f0c8ba91a3d85bd51a22b37
Andrea Cedraro <a.cedraro@gmail.com>
2024-01-14 15:35:48 +0100

    nvim: use native vim.base64 for osc52 clipboard

--- roles/neovim/files/nvim/lua/config/clipboard.lua
+++ roles/neovim/files/nvim/lua/config/clipboard.lua
@@ -1,27 +1,4 @@
--- character table string
-local N = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
--- encoding
-local function base64(data)
-  return ((data:gsub('.', function(x)
-    local r, b = '', x:byte()
-    for i = 8, 1, -1 do
-      r = r .. (b % 2^i - b % 2^(i-1) > 0 and '1' or '0')
-    end
-    return r
-  end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-      if #x < 6 then
-        return ''
-      end
-      local c = 0
-      for i = 1, 6 do
-        c = c + (x:sub(i, i) == '1' and 2^(6 - i) or 0)
-      end
-      return N:sub(c + 1, c + 1)
-    end)..({ '', '==', '=' })[#data%3+1])
-end
-
-local function ocs52clipboard()
+local function copy(register)
   local tmux_prefix = ""
   local tmux_postfix = ""
   if vim.env.TMUX ~= nil then
@@ -36,27 +13,28 @@ local function ocs52clipboard()
     }, '')
   end
 
+  local clipboard = register == '+' and 'c' or 'p'
   return function(lines, _)
     local text = table.concat(lines, "\n")
     local ocs52 = table.concat({
       tmux_prefix,
       string.char(0x1b),
-      "]52;c;",
-      base64(text),
+      "]52;",
+      clipboard,
+      ";",
+      vim.base64.encode(text),
       string.char(0x07),
       tmux_postfix,
     }, '')
 
-    if vim.fn.chansend(vim.v.stderr, ocs52) <= 0 then
-      vim.api.nvim_echo({{'Failed to copy selection', 'ErrorMsg'}}, false, {})
-    end
+    vim.api.nvim_chan_send(2, ocs52)
   end
 end
 vim.g.clipboard = {
   name = 'ocs52',
   copy = {
-    ['+'] = ocs52clipboard(),
-    ['*'] = ocs52clipboard(),
+    ['+'] = copy('+'),
+    ['*'] = copy('*'),
   },
   paste = { ['+'] = {}, ['*'] = {} },
   cache_enabled = 1
