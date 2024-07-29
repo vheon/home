ab86889e599fc42b75bba7eff4e49f38467d2b89
Andrea Cedraro <andrea.cedraro@hcl.software>
2024-07-29 22:38:20 +0200

    nvim: add which-key

--- roles/neovim/files/nvim/lua/plugins/init.lua
+++ roles/neovim/files/nvim/lua/plugins/init.lua
@@ -838,6 +838,21 @@ return {
     },
 
     {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+
+    {
         "stevearc/aerial.nvim",
         keys = {
             { "<leader>ot", "<cmd>AerialToggle<cr>", desc = "AerialToggle" },
